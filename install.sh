7c3874d07986b66b763e797b7f8597f839d502c3
HANNTF <140069500+HANNTF@users.noreply.github.com>
2023-07-23 13:36:36 +0700

    install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ ENDCOLOR="\e[0m"
 clear
 echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
 sleep 1
-if [ -f "AnchProxy" ]; then
+if [ -f "SourProxy" ]; then
     echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
-    rm AnchProxy
+    rm SourProxy 
     sleep 1
     echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
 fi
-wget -q https://github.com/AyamNubCh/AnchProxy/raw/main/AnchProxy
+wget -q https://github.com/HANNTF/SourProxy/raw/main/SourProxy
 sleep 1
 echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
-echo -e "${GREEN}Execute proxy with this command: ./AnchProxy${ENDCOLOR}"
-chmod +x AnchProxy
+echo -e "${GREEN}Execute proxy with this command: ./SourProxy${ENDCOLOR}"
+chmod +x SourProxy
